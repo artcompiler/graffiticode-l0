cf1eacb48d72bfa58a25867794afdc00f00351cb
Jeff Dyer <jeffdyer@acm.org>
2021-04-07 14:00:46 -0700

    Upgrade basis

--- src/compile.js
+++ src/compile.js
@@ -1,7 +1,7 @@
 /* Copyright (c) 2021, ARTCOMPILER INC */
 import {assert, message, messages, reserveCodeRange} from "./share.js";
-//import {BasisCompiler} from '@graffiticode/basis';
-import {BasisCompiler} from '../../../../work/graffiticode/basis/index.js';
+import {BasisCompiler} from '@graffiticode/basis';
+//import {BasisCompiler} from '../../../../work/graffiticode/basis/index.js';
 export const compiler = new BasisCompiler({
   langID: 0,
   version: 'v0.0.0',
